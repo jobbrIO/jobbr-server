dc39e6d74199f2db7f9abb34287dcdaf488fe661
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-03-04 18:33:27 +0100

    added demo solution

---
+++ demo/Demo.JobRunner/Program.cs
@@ -0,0 +1,21 @@
+﻿using Jobbr.Runtime;
+
+namespace Demo.JobRunner
+{
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            new JobbrRuntime().Run(args);
+        }
+    }
+}
---
+++ demo/Demo.JobServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Jobbr.Server;
+using Jobbr.Server.Dapper;
+
+namespace Demo.JobServer
+{
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            var dapperProvider = new JobbrDapperProvider(@"Data Source=.\SQLEXPRESS;Initial Catalog=JobbrDemo;Integrated Security=True");
+
+            var config = new JobbrConfiguration
+                             {
+                                 JobQueueProvider = dapperProvider, 
+                                 JobRepositoryProvider = dapperProvider,
+                                 JobRunnerExeResolver = () => "Demo.JobRunner.exe"
+                             };
+
+            using (var jobserver = new JobbrServer(config))
+            {
+                jobserver.Start();
+
+                Console.Write("JobServer has started. Press enter to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
