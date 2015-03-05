df71b70eb3e67c581cd085228f05b5cebe5a4948
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-03-05 11:31:26 +0100

    refactored to common

---
+++ source/Jobbr.Common/IJobbrDependencyResolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jobbr.Common
+{
+    /// <summary>
+    /// Represents an interface for the range of the dependencies.
+    /// </summary>
+    public interface IJobbrDependencyResolver
+    {
+        /// <summary>
+        /// Retrieves a service from the scope.
+        /// </summary>
+        /// <param name="serviceType">The service to be retrieved.</param>
+        /// <returns>The retrieved service.</returns>
+        object GetService(Type serviceType);
+        
+        /// <summary>
+        /// Retrieves a collection of services from the scope.
+        /// </summary>
+        /// <param name="serviceType"> The collection of services to be retrieved.</param>
+        /// <returns>The retrieved collection of services.</returns>
+        IEnumerable<object> GetServices(Type serviceType);
+    }
+}
