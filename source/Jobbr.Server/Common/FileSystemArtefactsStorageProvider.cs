24e1da660f4ef18b5b72c68984d643b0444a2c08
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-03-06 19:36:24 +0100

    save and load files and container contents

--- source/Jobbr.Server/Common/FileSystemArtefactsStorageProvider.cs
+++ source/Jobbr.Server/Common/FileSystemArtefactsStorageProvider.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+
+using Microsoft.SqlServer.Server;
 
 namespace Jobbr.Server.Common
 {
@@ -14,15 +18,40 @@ namespace Jobbr.Server.Common
             this.dataDirectory = dataDirectory;
         }
 
-        public void Save(string key, string fileName, Stream content)
+        public void Save(string container, string fileName, Stream content)
         {
-            var dir = Directory.CreateDirectory(Path.Combine(this.dataDirectory, key));
+            var dir = Directory.CreateDirectory(Path.Combine(this.dataDirectory, container));
+            var fileFullPath = Path.Combine(dir.FullName, fileName);
+
+            if (File.Exists(fileFullPath))
+            {
+                File.Delete(fileFullPath);
+            }
+
+            using (var fileStream = File.Create(fileFullPath))
+            {
+                content.CopyTo(fileStream);
+            }
         }
 
-        public Stream Load(string key, string fileName)
+        public Stream Load(string container, string fileName)
         {
-            var dir = Directory.CreateDirectory(Path.Combine(this.dataDirectory, key));
+            var dir = Directory.CreateDirectory(Path.Combine(this.dataDirectory, container));
+            var fileFullPath = Path.Combine(dir.FullName, fileName);
+
+            if (File.Exists(fileFullPath))
+            {
+                return File.OpenRead(fileFullPath);
+            }
+
             return null;
+        }
+
+        public List<FileInfo> GetFiles(string container)
+        {
+            var dir = Directory.CreateDirectory(Path.Combine(this.dataDirectory, container));
+
+            return dir.GetFiles().ToList();
         }
     }
 }
--- source/Jobbr.Server/Common/IArtefactsStorageProvider.cs
+++ source/Jobbr.Server/Common/IArtefactsStorageProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace Jobbr.Server.Common
 {
@@ -10,8 +11,8 @@ namespace Jobbr.Server.Common
         /// <summary>
         /// The save.
         /// </summary>
-        /// <param name="key">
-        /// The key.
+        /// <param name="container">
+        /// The container.
         /// </param>
         /// <param name="fileName">
         /// The file name.
@@ -19,13 +20,13 @@ namespace Jobbr.Server.Common
         /// <param name="content">
         /// The content.
         /// </param>
-        void Save(string key, string fileName, Stream content);
+        void Save(string container, string fileName, Stream content);
 
         /// <summary>
         /// The load.
         /// </summary>
-        /// <param name="key">
-        /// The key.
+        /// <param name="container">
+        /// The container.
         /// </param>
         /// <param name="fileName">
         /// The file name.
@@ -33,7 +34,8 @@ namespace Jobbr.Server.Common
         /// <returns>
         /// The <see cref="Stream"/>.
         /// </returns>
-        Stream Load(string key, string fileName);
-    
+        Stream Load(string container, string fileName);
+
+        List<FileInfo> GetFiles(string container);
     }
 }
