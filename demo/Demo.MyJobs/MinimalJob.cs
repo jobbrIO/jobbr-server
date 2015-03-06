9210926757f16fae0c140c956b851aa92b5a9672
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-03-06 15:17:24 +0100

    demojob

---
+++ demo/Demo.MyJobs/MinimalJob.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Demo.MyJobs
+{
+    /// <summary>
+    /// The minimal job.
+    /// </summary>
+    public class MinimalJob
+    {
+        /// <summary>
+        /// The run.
+        /// </summary>
+        public void Run()
+        {
+            File.WriteAllText("content.txt", "This is the content");
+        }
+    }
+}
