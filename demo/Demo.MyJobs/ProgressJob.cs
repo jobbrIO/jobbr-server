6d7404f36b79e7a000c284762c32554162554f1f
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-03-10 20:04:19 +0100

    new demojob

---
+++ demo/Demo.MyJobs/ProgressJob.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Demo.MyJobs
+{
+    public class ProgressJob
+    {
+        public void Run()
+        {
+            var iterations = 15;
+
+            for (var i = 0; i < iterations; i++)
+            {
+                Thread.Sleep(500);
+
+                Console.WriteLine("##jobbr[progress percent='{0:0.00}']", (double)(i + 1) / iterations * 100);
+            }
+        }
+    }
+}
