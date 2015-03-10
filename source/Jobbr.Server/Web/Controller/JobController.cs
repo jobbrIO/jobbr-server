5a02547cadc312f6592bf6a694a7a3a1cff4bfe7
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-03-10 20:12:20 +0100

    refa

--- source/Jobbr.Server/Web/Controller/JobController.cs
+++ source/Jobbr.Server/Web/Controller/JobController.cs
@@ -7,7 +7,6 @@ using System.Net.Http.Headers;
 using System.Web.Http;
 
 using Jobbr.Server.Common;
-using Jobbr.Server.Core;
 using Jobbr.Server.Model;
 using Jobbr.Server.Web.Dto;
 
@@ -22,15 +21,9 @@ namespace Jobbr.Server.Web.Controller
     {
         private readonly IJobStorageProvider jobStorageProvider;
 
-        private readonly IJobService jobService;
-
-        private readonly IArtefactsStorageProvider artefactsStorageProvider;
-
-        public JobController(IJobStorageProvider jobStorageProvider, IJobService jobService, IArtefactsStorageProvider artefactsStorageProvider)
+        public JobController(IJobStorageProvider jobStorageProvider)
         {
             this.jobStorageProvider = jobStorageProvider;
-            this.jobService = jobService;
-            this.artefactsStorageProvider = artefactsStorageProvider;
         }
 
         [HttpGet]
@@ -65,74 +58,6 @@ namespace Jobbr.Server.Web.Controller
             }
 
             return this.Ok(list);
-        }
-
-        [HttpGet]
-        [Route("api/jobRuns/{jobRunId}")]
-        public IHttpActionResult GetJonRun(long jobRunId)
-        {
-            var jobRun = this.jobStorageProvider.GetJobRuns().FirstOrDefault(jr => jr.Id == jobRunId);
-
-            if (jobRun == null)
-            {
-                return this.NotFound();
-            }
-
-            var jobParameter = jobRun.JobParameters != null ? JsonConvert.DeserializeObject(jobRun.JobParameters) : null;
-            var instanceParameter = jobRun.InstanceParameters != null ? JsonConvert.DeserializeObject(jobRun.InstanceParameters) : null;
-
-            var files = this.artefactsStorageProvider.GetFiles(jobRun.UniqueId);
-            var filesList = new List<JobRunArtefactDto>();
-
-            foreach (var fileInfo in files)
-            {
-                filesList.Add(new JobRunArtefactDto()
-                                  {
-                                      Filename = fileInfo.Name,
-                                      Size = fileInfo.Length,
-                                  });
-
-            }
-
-            var dto = new JobRunDto()
-                          {
-                              JobRunId = jobRun.Id,
-                              JobId = jobRun.JobId,
-                              TriggerId = jobRun.TriggerId,
-                              UniqueId = new Guid(jobRun.UniqueId),
-                              JobParameter = jobParameter,
-                              InstanceParameter = instanceParameter,
-                              State = jobRun.State.ToString(),
-                              Progress = jobRun.Progress,
-                              PlannedStartUtc = jobRun.PlannedStartDateTimeUtc,
-                              AuctualStartUtc = jobRun.ActualStartDateTimeUtc,
-                              EstimatedEndtUtc = jobRun.EstimatedEndDateTimeUtc,
-                              AuctualEndUtc = jobRun.ActualEndDateTimeUtc,
-                              Artefacts = filesList
-                          };
-            
-            return this.Ok(dto);
-        }
-
-        [HttpGet]
-        [Route("api/jobRuns/{jobRunId}/artefacts/{filename}")]
-        public IHttpActionResult GetArtefact(long jobRunId, string filename)
-        {
-            var jobRun = this.jobStorageProvider.GetJobRuns().FirstOrDefault(jr => jr.Id == jobRunId);
-
-            if (jobRun == null)
-            {
-                return this.NotFound();
-            }
-
-            var fileStream = this.artefactsStorageProvider.Load(jobRun.UniqueId, filename);
-
-            HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
-
-            result.Content = new StreamContent(fileStream);
-            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-
-            return this.ResponseMessage(result);
         }
     }
 }
---
+++ source/Jobbr.Server/Web/Controller/JobRunController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+using Jobbr.Server.Common;
+using Jobbr.Server.Web.Dto;
+
+using Newtonsoft.Json;
+
+namespace Jobbr.Server.Web.Controller
+{
+    public class JobRunController : ApiController
+    {
+        private IJobStorageProvider jobStorageProvider;
+
+        private IArtefactsStorageProvider artefactsStorageProvider;
+
+        public JobRunController(IJobStorageProvider jobStorageProvider, IArtefactsStorageProvider artefactsStorageProvider)
+        {
+            this.jobStorageProvider = jobStorageProvider;
+            this.artefactsStorageProvider = artefactsStorageProvider;
+        }
+
+        [HttpGet]
+        [Route("api/jobRuns/{jobRunId}")]
+        public IHttpActionResult GetJonRun(long jobRunId)
+        {
+            var jobRun = this.jobStorageProvider.GetJobRuns().FirstOrDefault(jr => jr.Id == jobRunId);
+
+            if (jobRun == null)
+            {
+                return this.NotFound();
+            }
+
+            var jobParameter = jobRun.JobParameters != null ? JsonConvert.DeserializeObject(jobRun.JobParameters) : null;
+            var instanceParameter = jobRun.InstanceParameters != null ? JsonConvert.DeserializeObject(jobRun.InstanceParameters) : null;
+
+            var files = this.artefactsStorageProvider.GetFiles(jobRun.UniqueId);
+            var filesList = files.Select(fileInfo => new JobRunArtefactDto() { Filename = fileInfo.Name, Size = fileInfo.Length, }).ToList();
+
+            var job = this.jobStorageProvider.GetJobById(jobRun.JobId);
+
+            var dto = new JobRunDto()
+            {
+                JobRunId = jobRun.Id,
+                JobId = jobRun.JobId,
+                JobName = job.Name,
+                TriggerId = jobRun.TriggerId,
+                UniqueId = new Guid(jobRun.UniqueId),
+                JobParameter = jobParameter,
+                InstanceParameter = instanceParameter,
+                State = jobRun.State.ToString(),
+                Progress = jobRun.Progress,
+                PlannedStartUtc = jobRun.PlannedStartDateTimeUtc,
+                AuctualStartUtc = jobRun.ActualStartDateTimeUtc,
+                EstimatedEndtUtc = jobRun.EstimatedEndDateTimeUtc,
+                AuctualEndUtc = jobRun.ActualEndDateTimeUtc,
+                Artefacts = filesList.Any() ? filesList : null
+            };
+
+            return this.Ok(dto);
+        }
+
+        [HttpGet]
+        [Route("api/jobRuns/{jobRunId}/artefacts/{filename}")]
+        public IHttpActionResult GetArtefact(long jobRunId, string filename)
+        {
+            var jobRun = this.jobStorageProvider.GetJobRuns().FirstOrDefault(jr => jr.Id == jobRunId);
+
+            if (jobRun == null)
+            {
+                return this.NotFound();
+            }
+
+            var fileStream = this.artefactsStorageProvider.Load(jobRun.UniqueId, filename);
+
+            HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+
+            result.Content = new StreamContent(fileStream);
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            return this.ResponseMessage(result);
+        }
+    
+    }
+}
