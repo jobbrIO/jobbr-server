007877cf534b658b51229fac5bf920a5a2b7601f
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-03-11 11:19:56 +0100

    r#

--- source/Jobbr.Runtime/JobbrRuntime.cs
+++ source/Jobbr.Runtime/JobbrRuntime.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlTypes;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
