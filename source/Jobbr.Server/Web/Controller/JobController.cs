44c5ec0b422a939607960e189dd4399c35fc855c
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-03-13 11:14:19 +0100

    extended to query by uniqueName

--- source/Jobbr.Server/Web/Controller/JobController.cs
+++ source/Jobbr.Server/Web/Controller/JobController.cs
@@ -12,6 +12,8 @@ using Newtonsoft.Json;
 
 namespace Jobbr.Server.Web.Controller
 {
+    using System.Net;
+
     /// <summary>
     /// The job controller.
     /// </summary>
@@ -38,36 +40,60 @@ namespace Jobbr.Server.Web.Controller
         [Route("api/jobs")]
         public IHttpActionResult AddJob([FromBody] JobDto dto)
         {
-            var job = new Job() { Name = dto.Name, Type = dto.Type, Parameters = JsonConvert.SerializeObject(dto.Parameters), };
+            var identifier = dto.UniqueName;
 
+            if (string.IsNullOrEmpty(dto.UniqueName))
+            {
+                return this.StatusCode(HttpStatusCode.NotAcceptable);
+            }
+
+            var existingJob = this.jobStorageProvider.GetJobByUniqueName(identifier);
+
+            if (existingJob != null)
+            {
+                return this.Conflict();
+            }
+
+            var job = new Job() { UniqueName = dto.UniqueName, Title = dto.Title, Type = dto.Type, Parameters = JsonConvert.SerializeObject(dto.Parameters), };
             var returnJob = this.service.AddJob(job);
 
-            return this.Ok(this.Map(returnJob));
+            return this.Created("/api/jobs/" + job.Id, this.Map(returnJob));
+        }
+
+        [HttpPost]
+        [Route("api/jobs/{jobId:long}")]
+        public IHttpActionResult UpdateJob(long jobId, [FromBody] JobDto dto)
+        {
+            var existingJob = this.jobStorageProvider.GetJobById(jobId);
+
+            if (existingJob == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.InternalServerError(new NotImplementedException());
         }
 
         [HttpGet]
-        [Route("api/jobs/{jobId}/runs")]
-        public IHttpActionResult GetJobRunsForJob(long jobId)
+        [Route("api/jobs/{jobId:long}/runs")]
+        public IHttpActionResult GetJobRunsForJobById(long jobId)
         {
             var jobRuns = this.jobStorageProvider.GetJobRuns().Where(jr => jr.JobId == jobId);
 
-            var list = new List<JobRunDto>();
+            var list = jobRuns.Select(MapJobRunForOverview).ToList();
 
-            foreach (var jobRun in jobRuns)
-            {
-                list.Add(new JobRunDto()
-                             {
-                                 JobRunId = jobRun.Id,
-                                 JobId = jobRun.JobId,
-                                 TriggerId = jobRun.TriggerId,
-                                 UniqueId = new Guid(jobRun.UniqueId),
-                                 State = jobRun.State.ToString(),
-                                 Progress = jobRun.Progress,
-                                 PlannedStartUtc = jobRun.PlannedStartDateTimeUtc,
-                                 AuctualStartUtc = jobRun.ActualStartDateTimeUtc,
-                                 AuctualEndUtc = jobRun.ActualEndDateTimeUtc,
-                             });
-            }
+            return this.Ok(list);
+        }
+
+        [HttpGet]
+        [Route("api/jobs/{uniqueName}/runs")]
+        public IHttpActionResult GetJobRunsForJobByUniqueName(string uniqueName)
+        {
+            var job = this.jobStorageProvider.GetJobByUniqueName(uniqueName);
+
+            var jobRuns = this.jobStorageProvider.GetJobRuns().Where(jr => jr.JobId == job.Id);
+
+            var list = jobRuns.Select(MapJobRunForOverview).ToList();
 
             return this.Ok(list);
         }
@@ -84,6 +110,22 @@ namespace Jobbr.Server.Web.Controller
                            UpdatedDateTimeUtc = job.UpdatedDateTimeUtc,
                            CreatedDateTimeUtc = job.CreatedDateTimeUtc
                        };
+        }
+
+        private static JobRunDto MapJobRunForOverview(JobRun jobRun)
+        {
+            return new JobRunDto()
+            {
+                JobRunId = jobRun.Id,
+                JobId = jobRun.JobId,
+                TriggerId = jobRun.TriggerId,
+                UniqueId = new Guid(jobRun.UniqueId),
+                State = jobRun.State.ToString(),
+                Progress = jobRun.Progress,
+                PlannedStartUtc = jobRun.PlannedStartDateTimeUtc,
+                AuctualStartUtc = jobRun.ActualStartDateTimeUtc,
+                AuctualEndUtc = jobRun.ActualEndDateTimeUtc,
+            };
         }
     }
 }
