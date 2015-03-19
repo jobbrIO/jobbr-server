29b9b30469a3522333c395f4b70ecbbc89c77650
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-03-19 10:45:15 +0100

    refa

---
+++ source/Jobbr.Server/Web/Mapping/JobMapper.cs
@@ -0,0 +1,42 @@
+using System;
+
+using Jobbr.Server.Model;
+using Jobbr.Server.Web.Dto;
+
+using Newtonsoft.Json;
+
+namespace Jobbr.Server.Web.Mapping
+{
+    public class JobMapper
+    {
+        public static JobDto Map(Job job)
+        {
+            return new JobDto()
+                       {
+                           Id = job.Id,
+                           UniqueName = job.UniqueName,
+                           Title = job.Title,
+                           Parameters = job.Parameters != null ? JsonConvert.DeserializeObject(job.Parameters) : null,
+                           Type = job.Type,
+                           UpdatedDateTimeUtc = job.UpdatedDateTimeUtc,
+                           CreatedDateTimeUtc = job.CreatedDateTimeUtc
+                       };
+        }
+
+        public static JobRunDto Map(JobRun jobRun)
+        {
+            return new JobRunDto()
+                       {
+                           JobRunId = jobRun.Id,
+                           JobId = jobRun.JobId,
+                           TriggerId = jobRun.TriggerId,
+                           UniqueId = new Guid(jobRun.UniqueId),
+                           State = jobRun.State.ToString(),
+                           Progress = jobRun.Progress,
+                           PlannedStartUtc = jobRun.PlannedStartDateTimeUtc,
+                           AuctualStartUtc = jobRun.ActualStartDateTimeUtc,
+                           AuctualEndUtc = jobRun.ActualEndDateTimeUtc,
+                       };
+        }
+    }
+}
