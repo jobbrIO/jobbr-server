966430d8522059324826963f2a0a5da9d58def42
Peter Gfader <pgf@zuehlke.com>
2015-03-23 08:15:22 +0100

    Added Specifications for Jobbr

---
+++ source/Jobbr.Tests/JobbrRuntimeTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jobbr.Tests
+{
+    [TestClass]
+    public class JobbrRuntimeSpecs
+    {
+        [TestMethod]
+        public void StartJob_WithParameters()
+        {
+
+        }
+
+        [TestMethod]
+        public void StartJob_With_OnlyInstanceParameters()
+        {
+        }
+        
+        [TestMethod]
+        public void StartJob_With_OnlyJobParameter()
+        {
+        }
+    }
+
+    public class DummyJob
+    {
+        public void Run()
+        {
+        }
+
+        public void Run(int one, int two)
+        {
+
+        }
+
+        public void Run(object jobParameters, CustomRunParameter customRunParameters)
+        {
+            Console.WriteLine("Got the params {0} and {1}", jobParameters, customRunParameters);
+        }
+    }
+
+
+    public class CustomRunParameter
+    {
+        public string Param1 { get; set; }
+
+        public int Param2 { get; set; }
+    }
+}
