9c9ee3d80998ed04184539dbe17c3dd89c784bb6
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-01-28 23:38:08 +0100

    updated build & nuget scripts

--- tools/ilmerge.bat
+++
@@ -1,6 +0,0 @@
-cd ..\source
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" Jobbr.Server.sln /Build "Release|Any CPU"
-
-packages\ILMerge.Tools.2.14.1208\tools\ilmerge.exe /out:../tools/Jobbr.Server.dll Jobbr.Server/bin/Release/*.dll /target:library /targetplatform:v4,C:\Windows\Microsoft.NET\Framework64\v4.0.30319 /wildcards /internalize:../tools/internalize_exclude.txt
-
-cd ..\tools
