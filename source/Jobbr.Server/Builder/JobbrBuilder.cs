fafb2414a11319ce605128c934b795455dbbb8cc
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-02-07 08:43:49 +0100

    inlined container so that the container will no be reused across jobbr-server instances

--- source/Jobbr.Server/Builder/JobbrBuilder.cs
+++ source/Jobbr.Server/Builder/JobbrBuilder.cs
@@ -14,63 +14,59 @@ namespace Jobbr.Server.Builder
     {
         private static readonly ILog Logger = LogProvider.For<JobbrBuilder>();
 
-        private readonly StandardKernel container;
-
-        public JobbrBuilder()
-        {
-            this.container = new DefaultContainer();
-        }
-
         public JobbrServer Create()
         {
+            StandardKernel container = new DefaultContainer();
+
             // Register default implementations if user did not specify any separate
-            if (this.container.TryGet<IJobStorageProvider>() == null)
+            if (container.TryGet<IJobStorageProvider>() == null)
             {
                 Logger.Error("There was no JobStorageProvider registered. Will continue building with an InMemory version, which does not support production scenarios.");
 
                 var inMemoryJobStorageProvider = new InMemoryJobStorageProvider();
-                this.container.Bind<IJobStorageProvider>().ToConstant(inMemoryJobStorageProvider);
+                container.Bind<IJobStorageProvider>().ToConstant(inMemoryJobStorageProvider);
             }
 
             // Register default implementations if user did not specify any separate
-            if (this.container.TryGet<IArtefactsStorageProvider>() == null)
+            if (container.TryGet<IArtefactsStorageProvider>() == null)
             {
                 Logger.Warn("There was no ArtefactsStorageProvider registered. Adding a default InMemoryArtefactStorage, which stores artefacts in memory. Please register a proper ArtefactStorage for production use.");
                 var fileSystemArtefactsStorageProvider = new InMemoryArtefactsStorage();
-                this.container.Bind<IArtefactsStorageProvider>().ToConstant(fileSystemArtefactsStorageProvider);
+                container.Bind<IArtefactsStorageProvider>().ToConstant(fileSystemArtefactsStorageProvider);
             }
 
             // Register default implementations if user did not specify any separate
-            if (this.container.TryGet<IJobExecutor>() == null)
+            if (container.TryGet<IJobExecutor>() == null)
             {
                 Logger.Error("There was no JobExecutor registered. Adding a Non-Operational JobExecutor");
-                this.container.Bind<IJobExecutor>().To<NoExecutor>();
+                container.Bind<IJobExecutor>().To<NoExecutor>();
             }
 
             // Register default implementations if user did not specify any separate
-            if (this.container.TryGet<IJobScheduler>() == null)
+            if (container.TryGet<IJobScheduler>() == null)
             {
                 // Don't warn because the internel Scheduler is usually in use
                 this.AddDefaultScheduler();
             }
 
-            return this.container.Get<JobbrServer>();
+            return container.Get<JobbrServer>();
         }
 
         public void Register<T>(Type type)
         {
-            this.container.Bind<T>().To(type).InSingletonScope();
+            new DefaultContainer().Bind<T>().To(type).InSingletonScope();
         }
 
         public void Add<T>(object instance)
         {
             var featureConfiguration = instance as IFeatureConfiguration;
+            StandardKernel container1 = new DefaultContainer();
             if (featureConfiguration != null)
             {
-                this.container.Bind<IFeatureConfiguration>().ToConstant((IFeatureConfiguration)instance);
+                container1.Bind<IFeatureConfiguration>().ToConstant((IFeatureConfiguration)instance);
             }
 
-            this.container.Bind<T>().ToConstant((T)instance);
+            container1.Bind<T>().ToConstant((T)instance);
         }
     }
 }
