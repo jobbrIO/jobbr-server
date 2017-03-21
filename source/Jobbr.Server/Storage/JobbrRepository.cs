f9d61ea1b65d3175b96a80181f8af9bf5a1f5ffc
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-03-21 17:08:11 +0100

    repo & interface moved into 2 different files

--- source/Jobbr.Server/Storage/JobbrRepository.cs
+++ source/Jobbr.Server/Storage/JobbrRepository.cs
@@ -6,66 +6,6 @@ using Jobbr.Server.Logging;
 
 namespace Jobbr.Server.Storage
 {
-    public interface IJobbrRepository
-    {
-        List<Job> GetAllJobs();
-
-        Job GetJob(long id);
-
-        void UpdateJobRunProgress(long jobRunId, double progress);
-
-        void SetPidForJobRun(JobRun jobRun, int id);
-
-        JobRun GetJobRun(long id);
-
-        List<JobTriggerBase> GetTriggers(long jobId);
-
-        void SaveAddTrigger(RecurringTrigger trigger);
-
-        void UpdatePlannedStartDateTimeUtc(long jobRunId, DateTime plannedStartDateTimeUtc);
-
-        void SaveAddTrigger(ScheduledTrigger trigger);
-
-        void SaveAddTrigger(InstantTrigger trigger);
-
-        JobRun GetLastJobRunByTriggerId(long triggerId);
-
-        JobRun GetNextJobRunByTriggerId(long triggerId);
-
-        bool EnableTrigger(long triggerId);
-
-        List<JobTriggerBase> GetActiveTriggers();
-
-        JobTriggerBase SaveUpdateTrigger(long id, JobTriggerBase trigger, out bool hadChanges);
-
-        bool CheckParallelExecution(long triggerId);
-
-        List<JobRun> GetJobRunsByState(JobRunStates state);
-
-        long AddJob(Job job);
-
-        JobRun SaveNewJobRun(Job job, JobTriggerBase trigger, DateTime plannedStartDateTimeUtc);
-
-        bool DisableTrigger(long triggerId);
-
-        void Update(JobRun jobRun);
-
-        JobRun GetJobRunById(long jobRunId);
-
-        JobTriggerBase GetTriggerById(long triggerId);
-
-        List<JobTriggerBase> GetTriggersByJobId(long jobId);
-
-        List<JobRun> GetAllJobRuns();
-
-        Job GetJobByUniqueName(string identifier);
-
-        void Delete(JobRun jobRun);
-        List<JobRun> GetJobRunsByTriggerId(long triggerId);
-        List<JobRun> GetJobRunsForUserId(long userId);
-        List<JobRun> GetJobRunsForUserName(string userName);
-    }
-
     public class JobbrRepository : IJobbrRepository
     {
         private static readonly ILog Logger = LogProvider.For<JobbrRepository>();
