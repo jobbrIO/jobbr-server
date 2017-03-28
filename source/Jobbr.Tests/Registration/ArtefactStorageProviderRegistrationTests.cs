9d026d703e21e8e8b0cd68949113dbbe01341b3c
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-03-28 11:23:00 +0200

    added test to validate logic for external file-storage providers is supported

---
+++ source/Jobbr.Tests/Registration/ArtefactStorageProviderRegistrationTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Jobbr.ComponentModel.ArtefactStorage;
+using Jobbr.ComponentModel.ArtefactStorage.Model;
+using Jobbr.ComponentModel.Registration;
+using Jobbr.Server.Builder;
+using Jobbr.Tests.Integration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jobbr.Tests.Registration
+{
+    [TestClass]
+    public class ArtefactStorageProviderRegistrationTests
+    {
+        [TestMethod]
+        public void BuilderHasCustomStorageProvider_AfterCreation_CorrectTypeIsActivated()
+        {
+            var builder = new JobbrBuilder();
+
+            builder.Register<IArtefactsStorageProvider>(typeof(CustomArtefactStorageAdapter));
+            builder.Register<IJobbrComponent>(typeof(ExposeAllServicesComponent));
+
+            var server = builder.Create();
+
+            Assert.IsNotNull(ExposeAllServicesComponent.Instance.ArtefactsStorageProvider);
+            Assert.AreEqual(typeof(CustomArtefactStorageAdapter), ExposeAllServicesComponent.Instance.ArtefactsStorageProvider.GetType());
+        }
+    }
+
+    public class CustomArtefactStorageAdapter : IArtefactsStorageProvider
+    {
+        public void Save(string container, string fileName, Stream content)
+        {
+        }
+
+        public Stream Load(string container, string fileName)
+        {
+            return null;
+        }
+
+        public List<JobbrArtefact> GetArtefacts(string container)
+        {
+            return null;
+        }
+    }
+}
