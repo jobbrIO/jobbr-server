29d6869f4059d1499e2825fed7c9100d741e1b72
Michael Schnyder <michael.schnyder@outlook.com>
2017-04-10 23:08:49 +0200

    avoid douplicate enummeration

--- source/Jobbr.Server/Builder/AutoMapperConfigurationFactory.cs
+++ source/Jobbr.Server/Builder/AutoMapperConfigurationFactory.cs
@@ -13,24 +13,25 @@ namespace Jobbr.Server.Builder
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "We want to express the dynamic aspect of it")]
         public MapperConfiguration GetNew()
         {
+            var foundAutoMapperProfiles = new List<Profile>();
+
             var profileTypes = this.GetType().Assembly.GetTypes().Where(t => t.Namespace != null && t.Namespace.StartsWith("Jobbr.Server") && typeof(Profile).IsAssignableFrom(t) && !t.IsAbstract);
+            var profileTypesList = profileTypes.ToList();
 
-            var profiles = new List<Profile>();
+            Logger.Debug($"Found {profileTypesList.Count} types that need to be registered in internal AutoMapper.");
 
-            Logger.Debug($"Found {profileTypes.Count()} types that need to be registered in internal AutoMapper.");
-
-            foreach (var profileType in profileTypes)
+            foreach (var profileType in profileTypesList)
             {
                 Logger.Debug($"Activating type '{profileType.Name}' from namespace '{profileType.Namespace}' in assembly '{profileType.Assembly}'");
 
                 // Don't try/catch here, better fail early (in the creation of Jobbr server)
                 var profile = (Profile) Activator.CreateInstance(profileType);
-                profiles.Add(profile);
+                foundAutoMapperProfiles.Add(profile);
             }
 
             var config = new MapperConfiguration(cfg =>
             {
-                foreach (var profile in profiles)
+                foreach (var profile in foundAutoMapperProfiles)
                 {
                     cfg.AddProfile(profile);
                 }
