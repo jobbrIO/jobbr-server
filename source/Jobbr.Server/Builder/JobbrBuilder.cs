351a968c9ed1e106b78b7314b85122360db72780
Michael Schnyder <michael.schnyder@outlook.com>
2017-04-11 18:02:00 +0200

    disabled CA regarding missing dipose, see comment

--- source/Jobbr.Server/Builder/JobbrBuilder.cs
+++ source/Jobbr.Server/Builder/JobbrBuilder.cs
@@ -10,7 +10,9 @@ using Ninject;
 
 namespace Jobbr.Server.Builder
 {
+    #pragma warning disable CA1001 // Don't implement IDisposable here because we cannot Dispose the container since the container is passed to the JobbrServer instance
     public class JobbrBuilder : IJobbrBuilder
+    #pragma warning restore CA1001 // Types that own disposable fields should be disposable
     {
         private static readonly ILog Logger = LogProvider.For<JobbrBuilder>();
         private readonly StandardKernel container;
