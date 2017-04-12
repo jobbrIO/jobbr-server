649beaaa439186bcacb5cd30425e52c5c7a84a01
Michael Schnyder <michael.schnyder@outlook.com>
2017-04-12 22:32:40 +0200

    added suppresions for external code

--- source/Jobbr.Server/GlobalSuppressions.cs
+++ source/Jobbr.Server/GlobalSuppressions.cs
@@ -3,4 +3,6 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1012:Abstract classes should not have public constructors", Justification = "TinyMessenger is imported code and managed from external developers", Scope = "type", Target = "~T:TinyMessenger.TinyMessageBase")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1012:Abstract classes should not have public constructors", Justification = "TinyMessenger is imported code and managed from external developers", Scope = "Type", Target = "TinyMessenger.TinyMessageBase")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2213:Disposable fields should be disposed", Justification = "TinyMessenger is imported code and managed from external developers", Scope = "Type", Target = "TinyMessenger.TinyMessengerHub")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2213:Disposable fields should be disposed", Justification = "LibLog is imported code and managed from external developers", Scope = "Type", Target = "Jobbr.Server.Logging.LogProviders.LogProviderBase")]
