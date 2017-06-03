2fa55dc162270be4a94476d5c0081855f2a94fa8
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-06-03 19:08:16 +0200

    throw exceptions that showed up dirung startup and fail, related to https://github.com/jobbrIO/jobbr-execution-forked/issues/1

--- source/Jobbr.Tests/Integration/Startup/BadEnvironmentTests.cs
+++ source/Jobbr.Tests/Integration/Startup/BadEnvironmentTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using Jobbr.ComponentModel.JobStorage;
+using Jobbr.ComponentModel.Registration;
 using Jobbr.Server;
 using Jobbr.Server.Builder;
 using Jobbr.Tests.Infrastructure.StorageProvider;
@@ -44,6 +45,38 @@ namespace Jobbr.Tests.Integration.Startup
             Assert.AreEqual(JobbrState.Running, jobbr.State);
         }
 
+        [TestMethod]
+        public void StartingJobbr_ComponentFails_IsInErrorState()
+        {
+            var builder = new JobbrBuilder();
+            builder.Register<IJobbrComponent>(typeof(FaultyComponent));
+
+            var jobbr = builder.Create();
+
+            try
+            {
+                jobbr.Start();
+            }
+            catch (Exception)
+            {
+                // Eat exception if any to check for the state
+            }
+
+            Assert.AreEqual(JobbrState.Error, jobbr.State);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void StartingJobbr_ComponentFails_ExceptionIsThrown()
+        {
+            var builder = new JobbrBuilder();
+            builder.Register<IJobbrComponent>(typeof(FaultyComponent));
+
+            var jobbr = builder.Create();
+
+            jobbr.Start();
+        }
+
         private void WaitForStatusChange(Func<JobbrState> state, int timeout)
         {
             var initialState = state();
@@ -58,6 +91,22 @@ namespace Jobbr.Tests.Integration.Startup
                 }
 
                 Thread.Sleep(500);
+            }
+        }
+
+        public class FaultyComponent : IJobbrComponent
+        {
+            public void Dispose()
+            {
+            }
+
+            public void Start()
+            {
+                throw new Exception();
+            }
+
+            public void Stop()
+            {
             }
         }
     }
