1d525a49d0a25c2a79d42027db6d88328a8ee2ff
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-06-05 19:30:49 +0200

    added test that shows that a couple of JobRunStates are missing and have shifted. relates to #42

---
+++ source/Jobbr.Tests/Integration/MappingTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jobbr.Tests.Integration
+{
+    [TestClass]
+    public class MappingTests
+    {
+        [TestMethod]
+        public void CoreRunStateNames_IfExistInComponentModels_NameHasSameValue()
+        {
+            var coreType = typeof(Jobbr.Server.Core.Models.JobRunStates);
+            var coreMembers = Enum.GetNames(coreType);
+
+            var errors = new List<string>();
+
+            var allJobJobRunEnumTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(asm => asm.GetTypes().Where(t => t.Namespace != null && t.IsEnum && t.Namespace.Contains("ComponentModel")));
+
+            foreach (var enumType in allJobJobRunEnumTypes)
+            {
+                var remoteMembers = Enum.GetNames(enumType);
+
+                foreach (var coreName in coreMembers)
+                {
+                    if (remoteMembers.Contains(coreName))
+                    {
+                        // AutoMapper maps by using the index/value of the enum so we do
+                        var coreValue = (int)Enum.Parse(coreType, coreName);
+                        var remoteVaue = (int)Enum.Parse(enumType, coreName);
+
+                        if (remoteVaue != coreValue)
+                        {
+                            var err = $"{enumType.FullName} contains a member for {coreName} but its value is different. CoreValue: {coreValue}, RemoteValue: {remoteVaue}";
+                            errors.Add(err);
+                        }
+                    }
+                }
+            }
+
+            Assert.AreEqual(0, errors.Count, $"One or more errors found while comparing component model values for {coreType}\n\n" + string.Join("\n", errors));
+
+        }
+    }
+}
