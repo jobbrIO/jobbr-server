e72b4f3ab5b4616477d813ee79cf74a6f1567f91
Michael Schnyder <michael.schnyder@outlook.com>
2017-06-07 23:03:16 +0200

    rafe

--- source/Jobbr.Tests/Integration/MappingTests.cs
+++ source/Jobbr.Tests/Integration/MappingTests.cs
@@ -8,39 +8,52 @@ namespace Jobbr.Tests.Integration
     [TestClass]
     public class MappingTests
     {
+        private readonly IEnumerable<Type> allComponentModelJobRunEnumTypes;
+        private readonly Type coreType;
+
+        public MappingTests()
+        {
+            this.allComponentModelJobRunEnumTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(asm => asm.GetTypes().Where(t => t.Namespace != null && t.IsEnum && t.Namespace.Contains("ComponentModel")));
+            this.coreType = typeof(Jobbr.Server.Core.Models.JobRunStates);
+        }
+
         [TestMethod]
         public void CoreRunStateNames_IfExistInComponentModels_NameHasSameValue()
         {
-            var coreType = typeof(Jobbr.Server.Core.Models.JobRunStates);
-            var coreMembers = Enum.GetNames(coreType);
-
             var errors = new List<string>();
 
-            var allJobJobRunEnumTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(asm => asm.GetTypes().Where(t => t.Namespace != null && t.IsEnum && t.Namespace.Contains("ComponentModel")));
-
-            foreach (var enumType in allJobJobRunEnumTypes)
+            foreach (var componentModelEnumType in this.allComponentModelJobRunEnumTypes)
             {
-                var remoteMembers = Enum.GetNames(enumType);
+                var differences = FindDifferentValues(this.coreType, componentModelEnumType);
 
-                foreach (var coreName in coreMembers)
-                {
-                    if (remoteMembers.Contains(coreName))
-                    {
-                        // AutoMapper maps by using the index/value of the enum so we do
-                        var coreValue = (int)Enum.Parse(coreType, coreName);
-                        var remoteVaue = (int)Enum.Parse(enumType, coreName);
-
-                        if (remoteVaue != coreValue)
-                        {
-                            var err = $"{enumType.FullName} contains a member for {coreName} but its value is different. CoreValue: {coreValue}, RemoteValue: {remoteVaue}";
-                            errors.Add(err);
-                        }
-                    }
-                }
+                errors.AddRange(differences);
             }
 
-            Assert.AreEqual(0, errors.Count, $"One or more errors found while comparing component model values for {coreType}\n\n" + string.Join("\n", errors));
+            Assert.AreEqual(0, errors.Count, $"One or more errors found while comparing component model values for {this.coreType}\n\n" + string.Join("\n", errors));
 
+        }
+
+        private static IEnumerable<string> FindDifferentValues(Type masterType, Type remoteType)
+        {
+            var remoteMembers = Enum.GetNames(remoteType);
+
+            foreach (var coreName in Enum.GetNames(masterType))
+            {
+                // Only check those member that also exist in remoteType
+                if (!remoteMembers.Contains(coreName)) continue;
+
+                // AutoMapper maps by using the index/value of the enum so we do
+                var coreValue = (int) Enum.Parse(masterType, coreName);
+                var remoteVaue = (int) Enum.Parse(remoteType, coreName);
+
+                if (remoteVaue != coreValue)
+                {
+                    var err = $"{remoteType.FullName} contains a member for {coreName} but its value is different. CoreValue: {coreValue}, RemoteValue: {remoteVaue}";
+
+                    yield return err;
+
+                }
+            }
         }
     }
 }
