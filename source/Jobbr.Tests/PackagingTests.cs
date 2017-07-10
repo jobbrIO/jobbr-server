fd27adecb867a4c2829c113867bf28eb3e5385e8
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-07-10 17:22:36 +0200

    added tests

---
+++ source/Jobbr.Tests/PackagingTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using Jobbr.DevSupport.ReferencedVersionAsserter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jobbr.Tests
+{
+    [TestClass]
+    public class PackagingTests
+    {
+        private readonly bool isPre = Assembly.GetExecutingAssembly().GetInformalVersion().Contains("-");
+
+        [TestMethod]
+        public void Feature_NuSpec_IsCompliant()
+        {
+            var asserter = new Asserter(Asserter.ResolvePackagesConfig("Jobbr.Server"), Asserter.ResolveRootFile("Jobbr.Server.nuspec"));
+
+            asserter.Add(new PackageExistsInBothRule("Jobbr.ComponentModel.ArtefactStorage"));
+            asserter.Add(new PackageExistsInBothRule("Jobbr.ComponentModel.Execution"));
+            asserter.Add(new PackageExistsInBothRule("Jobbr.ComponentModel.JobStorage"));
+            asserter.Add(new PackageExistsInBothRule("Jobbr.ComponentModel.Management"));
+            asserter.Add(new PackageExistsInBothRule("Jobbr.ComponentModel.Registration"));
+
+            if (this.isPre)
+            {
+                // This rule is only valid for Pre-Release versions because we only need exact match on PreRelease Versions
+                asserter.Add(new ExactVersionMatchRule("Jobbr.ComponentModel.*"));
+            }
+            else
+            {
+                asserter.Add(new AllowNonBreakingChanges("Jobbr.ComonentModel.*"));
+            }
+
+            asserter.Add(new VersionIsIncludedInRange("Jobbr.ComponentModel.*"));
+            asserter.Add(new NoMajorChangesInNuSpec("Jobbr.*"));
+
+            var result = asserter.Validate();
+
+            Assert.IsTrue(result.IsSuccessful, result.Message);
+        }
+    }
+}
