c714a7a373754189ad256ec1721bfcc63b290cc7
Steven Giesel <stgiesel35@gmail.com>
2019-04-15 16:34:01 +0200

    Removed unused using

--- source/Jobbr.Server/JobRegistry/RegistryBuilder.cs
+++ source/Jobbr.Server/JobRegistry/RegistryBuilder.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using Jobbr.ComponentModel.JobStorage;
 using Jobbr.ComponentModel.JobStorage.Model;
