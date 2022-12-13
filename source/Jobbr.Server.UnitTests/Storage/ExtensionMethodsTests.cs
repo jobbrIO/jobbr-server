9104d94132f6f3f83694f822e309cf6463120514
Loris Keller <loris.keller@zuehlke.com>
2022-12-13 15:19:54 +0100

    add missing using

--- source/Jobbr.Server.UnitTests/Storage/ExtensionMethodsTests.cs
+++ source/Jobbr.Server.UnitTests/Storage/ExtensionMethodsTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.Serialization;
 using Jobbr.Server.Storage;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
