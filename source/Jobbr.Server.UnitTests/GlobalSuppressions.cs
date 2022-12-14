5ca0c698657793b3ce96389c8e619983cd8b149d
Loris Keller <loris.keller@zuehlke.com>
2022-12-14 11:46:38 +0100

    suspend documentation warning for the whole project

--- source/Jobbr.Server.UnitTests/GlobalSuppressions.cs
+++ source/Jobbr.Server.UnitTests/GlobalSuppressions.cs
@@ -5,4 +5,4 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "type", Target = "~T:Jobbr.Server.UnitTests.Core.TriggerServiceTests")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "No documentation needed for test classes")]
